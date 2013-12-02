6cb1c3588adf707d38c006e7ccabcc9291c6093a
driverblock <rick@embedded-designs.com>
2013-12-02 11:25:03 -0500

    Bug Fix: print(0) printed blanks
    printFloat failed when displayNumber was 0. It only printed blanks, no '0'. The for loop would exit immediately when displayNumber was 0.
    Rather than monkeying with the loop itself, I simply added a special case for 0

--- Adafruit_LEDBackpack.cpp
+++ Adafruit_LEDBackpack.cpp
@@ -331,11 +331,17 @@ void Adafruit_7segment::printFloat(double n, uint8_t fracDigits, uint8_t base)
     // otherwise, display the number
     int8_t displayPos = 4;
     
-    for(uint8_t i = 0; displayNumber; ++i) {
-      boolean displayDecimal = (fracDigits != 0 && i == fracDigits);
-      writeDigitNum(displayPos--, displayNumber % base, displayDecimal);
-      if(displayPos == 2) writeDigitRaw(displayPos--, 0x00);
-      displayNumber /= base;
+    if (displayNumber)  //if displayNumber is not 0
+    {
+      for(uint8_t i = 0; displayNumber; ++i) {
+        boolean displayDecimal = (fracDigits != 0 && i == fracDigits);
+        writeDigitNum(displayPos--, displayNumber % base, displayDecimal);
+        if(displayPos == 2) writeDigitRaw(displayPos--, 0x00);
+        displayNumber /= base;
+      }
+    }
+    else {
+      writeDigitNum(displayPos--, 0, false);
     }
   
     // display negative sign if negative
