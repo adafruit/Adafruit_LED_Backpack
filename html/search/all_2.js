48efd0f0e24f6c95ba4d0b496f50eb63ace7adf6
Doxygen CI <ci-arduino@invalid>
2021-04-04 00:37:24 +0000

    Deploy docs to GitHub Pages from commit 3818082ec2
    Commit: 3818082ec2c5d1779a8e3acad69cca3c9c0f14c6
    GitHub Actions run: 715487034

--- html/search/all_2.js
+++ html/search/all_2.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['begin',['begin',['../class_adafruit___l_e_d_backpack.html#a1e3c6e924921dab0f4643955b7ce356b',1,'Adafruit_LEDBackpack']]],
+  ['begin',['begin',['../class_adafruit___l_e_d_backpack.html#ae7e1187688d9f0ca3cd2f7ab52b4e46c',1,'Adafruit_LEDBackpack']]],
   ['bin',['BIN',['../_adafruit___l_e_d_backpack_8h.html#a75267cdfa3fa9e52c7c1f1094f9387b7',1,'Adafruit_LEDBackpack.h']]],
   ['blinkrate',['blinkRate',['../class_adafruit___l_e_d_backpack.html#a1149eda49278d0be6d86fdeca668f11b',1,'Adafruit_LEDBackpack']]],
   ['byte',['BYTE',['../_adafruit___l_e_d_backpack_8h.html#aec93e83855ac17c3c25c55c37ca186dd',1,'Adafruit_LEDBackpack.h']]]
