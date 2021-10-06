16c256cdb44a168cde11606bf18794a3e3298fc8
Doxygen CI <ci-arduino@invalid>
2021-10-06 22:50:34 +0000

    Deploy docs to GitHub Pages from commit a2d9473adf
    Commit: a2d9473adfa88a06324066c350302394546dae2d
    GitHub Actions run: 1313955797

--- html/search/all_b.js
+++ html/search/all_b.js
@@ -1,8 +1,8 @@
 var searchData=
 [
-  ['write',['write',['../class_adafruit__7segment.html#a66759fcb5fc31e3c803adbd0759c4086',1,'Adafruit_7segment']]],
+  ['write',['write',['../class_adafruit__7segment.html#a0a752d7b8d8fb04060f18704b4283431',1,'Adafruit_7segment::write(char c)'],['../class_adafruit__7segment.html#ad492f6078ecf835eec926ef9cf67b60e',1,'Adafruit_7segment::write(const char *buffer, size_t size)']]],
   ['writecolon',['writeColon',['../class_adafruit__7segment.html#a6a81cefe40b5dddedf625ca5fcaa1128',1,'Adafruit_7segment']]],
-  ['writedigitascii',['writeDigitAscii',['../class_adafruit___alpha_num4.html#afe595f9bc2a15c0ba988eda75aa12420',1,'Adafruit_AlphaNum4']]],
+  ['writedigitascii',['writeDigitAscii',['../class_adafruit__7segment.html#a3b6ee7ad773c6a229aa0bd75953223d0',1,'Adafruit_7segment::writeDigitAscii()'],['../class_adafruit___alpha_num4.html#afe595f9bc2a15c0ba988eda75aa12420',1,'Adafruit_AlphaNum4::writeDigitAscii()']]],
   ['writedigitnum',['writeDigitNum',['../class_adafruit__7segment.html#a279224a72edaee4f38ac8d1f1dc41391',1,'Adafruit_7segment']]],
   ['writedigitraw',['writeDigitRaw',['../class_adafruit__7segment.html#aa584dbe79c196ea7f10df654d4572bee',1,'Adafruit_7segment::writeDigitRaw()'],['../class_adafruit___alpha_num4.html#aa26f3abf0a73b1c85375efeacdae48a7',1,'Adafruit_AlphaNum4::writeDigitRaw()']]],
   ['writedisplay',['writeDisplay',['../class_adafruit___l_e_d_backpack.html#a847e2e9cfb838163cd9d53750bc4dee0',1,'Adafruit_LEDBackpack']]]
