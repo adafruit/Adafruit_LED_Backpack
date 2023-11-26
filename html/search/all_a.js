dac143ca39856324aa05103d7b4ae62de8f3225a
Doxygen CI <ci-arduino@invalid>
2023-11-26 15:12:06 +0000

    Deploy docs to GitHub Pages from commit bf0d86238a
    Commit: bf0d86238ac11655da4047c11564d692ce4bb12d
    GitHub Actions run: 6996369448

--- html/search/all_a.js
+++ html/search/all_a.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['setbar',['setBar',['../class_adafruit__24bargraph.html#a47b435184547c666be21d658cb88993b',1,'Adafruit_24bargraph']]],
   ['setbrightness',['setBrightness',['../class_adafruit___l_e_d_backpack.html#a88fb0e40dd954b4fa885ded84acdc41e',1,'Adafruit_LEDBackpack']]],
+  ['setdisplaystate',['setDisplayState',['../class_adafruit___l_e_d_backpack.html#a7e9ee26c80d98ccf729093dc826ff6fd',1,'Adafruit_LEDBackpack']]],
   ['sevenseg_5fdigits',['SEVENSEG_DIGITS',['../_adafruit___l_e_d_backpack_8h.html#a5b377c04c67d8f647cfcf89452074893',1,'Adafruit_LEDBackpack.h']]]
 ];
--- html/search/functions_5.js
+++ html/search/functions_5.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['setbar',['setBar',['../class_adafruit__24bargraph.html#a47b435184547c666be21d658cb88993b',1,'Adafruit_24bargraph']]],
-  ['setbrightness',['setBrightness',['../class_adafruit___l_e_d_backpack.html#a88fb0e40dd954b4fa885ded84acdc41e',1,'Adafruit_LEDBackpack']]]
+  ['setbrightness',['setBrightness',['../class_adafruit___l_e_d_backpack.html#a88fb0e40dd954b4fa885ded84acdc41e',1,'Adafruit_LEDBackpack']]],
+  ['setdisplaystate',['setDisplayState',['../class_adafruit___l_e_d_backpack.html#a7e9ee26c80d98ccf729093dc826ff6fd',1,'Adafruit_LEDBackpack']]]
 ];
